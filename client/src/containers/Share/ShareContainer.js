807f49a49a1ab39b247f5b654193888407426ca1
Lindsey <lindsey@users-MBP.hitronhub.home>
2018-01-31 20:49:03 -0800

    added signout and profile button logic with firebase

--- client/src/containers/Share/ShareContainer.js
+++ client/src/containers/Share/ShareContainer.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import Share from './Share';
 import ShareItemCard from '../../components/ShareItemCard';
 import './styles.css';
+import { firebaseAuth } from '../../config/firebaseConfig';
 
 export default class ShareContainer extends Component {
     render() {
         return (
             <div className="share-wrapper">
                 <div className="share-item-card-wrapper">
-                    <ShareItemCard owner={'LAi9TYWxgGhbjgHu1Sm6ZvB1tRP2'} />
+                    <ShareItemCard owner={'firebaseAuth.currentUser.uid'} />
                 </div>
                 <div>
                     <Share />
